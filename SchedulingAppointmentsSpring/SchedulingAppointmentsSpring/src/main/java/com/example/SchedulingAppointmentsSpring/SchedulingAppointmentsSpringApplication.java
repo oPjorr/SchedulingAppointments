69274f5aa5761857626186a78436e5f517fd1db7
Paulo <paulorolinski.ifc@gmail.com>
2025-01-06 15:49:53 -0300

    feat: base project

---
+++ SchedulingAppointmentsSpring/SchedulingAppointmentsSpring/src/main/java/com/example/SchedulingAppointmentsSpring/SchedulingAppointmentsSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.SchedulingAppointmentsSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulingAppointmentsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulingAppointmentsSpringApplication.class, args);
+	}
+
+}
---
+++ SchedulingAppointmentsSpring/SchedulingAppointmentsSpring/src/test/java/com/example/SchedulingAppointmentsSpring/SchedulingAppointmentsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SchedulingAppointmentsSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulingAppointmentsSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

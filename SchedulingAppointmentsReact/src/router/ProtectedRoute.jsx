38d95de84987ee3ca51518ac774556252b412391
Paulo <paulorolinski.ifc@gmail.com>
2025-01-15 15:07:36 -0300

    chore: changing the redirecting url

--- SchedulingAppointmentsReact/src/router/ProtectedRoute.jsx
+++ SchedulingAppointmentsReact/src/router/ProtectedRoute.jsx
@@ -2,7 +2,7 @@ import { Navigate, Outlet } from "react-router-dom";
 import checkToken from "../service/auth/checkToken";
 
 const ProtectedRoute = () => {
-  return checkToken() ? <Outlet/> : <Navigate to="/login" replace />;
+  return checkToken() ? <Outlet/> : <Navigate to="/" replace />;
 }
 
 export default ProtectedRoute;
